b7783ac6cee3ea1bb8eb667739fa26c1fd42c568
BrowserNeo <browserneo@email.com>
2025-03-06 23:13:35 -0500

    ginPage

---
+++ 5_JavaScript_3/src/pages/loginPage.js
@@ -0,0 +1,20 @@
+class LoginPage {
+    constructor(page) {
+      this.page = page;
+      this.emailInput = page.locator('input[placeholder="Email"]');
+      this.passwordInput = page.locator('input[placeholder="Password"]');
+      this.signInButton = page.locator('button', { hasText: 'Sign in' });
+    }
+  
+    async goto() {
+      await this.page.goto('/login');
+    }
+  
+    async login(email, password) {
+      await this.emailInput.fill(email);
+      await this.passwordInput.fill(password);
+      await this.signInButton.click();
+    }
+  }
+  
+  module.exports = { LoginPage };
