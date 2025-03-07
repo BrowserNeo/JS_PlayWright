cfd300ad8625b84af94d430b7835358550abebe4
BrowserNeo <browserneo@email.com>
2025-03-06 23:19:13 -0500

    add testData

---
+++ 5_JavaScript_3/utils/testData.js
@@ -0,0 +1,15 @@
+module.exports = {
+    user: {
+      email: 'testuser@example.com',
+      password: 'OldPassword123!',
+      newPassword: 'NewPassword123!',
+      username: 'testuser',
+    },
+    article: {
+      title: 'Test Article',
+      description: 'This is a test article',
+      body: 'Lorem ipsum dolor sit amet.',
+      tags: 'test',
+    },
+    comment: 'Great article!',
+  };
